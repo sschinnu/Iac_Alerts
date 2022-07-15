0ca380590ddd5bb8f98456ad118ba464dd9b7b98
sschinnu <109149371+sschinnu@users.noreply.github.com>
2022-07-15 10:34:10 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "mfadelete" {
-  bucket = "examplebuckettftest"
+resource "aws_s3_bucket" "mfadelete15" {
+  bucket = "examplebuckettftest15"
   acl    = "private"
   versioning {
     enabled = true
@@ -7,55 +7,3 @@ resource "aws_s3_bucket" "mfadelete" {
   }
 }
 
-resource "aws_s3_bucket" "mfadelete2" {
-  bucket = "examplebuckettftest2"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete3" {
-  bucket = "examplebuckettftest3"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete4" {
-  bucket = "examplebuckettftest4"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}
-resource "aws_s3_bucket" "mfadelete5" {
-  bucket = "examplebuckettftest5"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete610" {
-  bucket = "examplebuckettftest610"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete9" {
-  bucket = "examplebuckettftest9"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}

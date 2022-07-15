3f2c7c178b9303b460c6e2962c6ce435258b64e9
sschinnu <109149371+sschinnu@users.noreply.github.com>
2022-07-15 11:39:26 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -1,11 +1,4 @@
-resource "aws_s3_bucket" "mfadelete15" {
-  bucket = "examplebuckettftest15"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
+
 resource "aws_s3_bucket" "mfadelete16" {
   bucket = "examplebuckettftest16"
   acl    = "private"
